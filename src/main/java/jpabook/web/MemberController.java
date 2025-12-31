c945e9f8b15a2d5de64ead330325bcf368e3ed9e
Mseunghwan <minccc42710@gmail.com>
2026-01-01 00:44:01 +0900

    7강 : 웹 계층 개발 done

---
+++ src/main/java/jpabook/web/MemberController.java
@@ -0,0 +1,53 @@
+package jpabook.web;
+
+import jakarta.validation.Valid;
+import jpabook.domain.Address;
+import jpabook.domain.Member;
+import jpabook.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 폼으로 이동
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    // 회원 생성
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
+
+    // 회원 목록 조회
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+}
